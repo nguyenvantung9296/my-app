16d253fec08531dda8bb58ae08b1dcb6c64531fc
nguyenvantung9296 <tungnguyenvan9296@gmail.com>
2020-07-25 00:24:36 +0700

    Updates

---
+++ precache-manifest.8a0ffd675a8d08635216028166aebfec.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37adb6d2c7739b391a0540176af0241",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "ed54c58e4ff79921e4e4",
+    "url": "/my-app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "cae5f117e40c6f8eca42",
+    "url": "/my-app/static/css/main.84a1c384.chunk.css"
+  },
+  {
+    "revision": "ed54c58e4ff79921e4e4",
+    "url": "/my-app/static/js/2.77d0b1ad.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-app/static/js/2.77d0b1ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cae5f117e40c6f8eca42",
+    "url": "/my-app/static/js/main.ee52ddbd.chunk.js"
+  },
+  {
+    "revision": "5db4ee20b3bc870ebba1",
+    "url": "/my-app/static/js/runtime-main.3a315cac.js"
+  },
+  {
+    "revision": "995cfe942612766b71d9a8689063752a",
+    "url": "/my-app/static/media/171-512.995cfe94.png"
+  },
+  {
+    "revision": "1c00c84a879de7c93e1f30510cd0f718",
+    "url": "/my-app/static/media/edit.1c00c84a.svg"
+  },
+  {
+    "revision": "b1f3fb7606c3c7ddf324e1a4df888ea5",
+    "url": "/my-app/static/media/j6a-pc-banner.b1f3fb76.jpg"
+  },
+  {
+    "revision": "4323a071a028e4f5e95fe971c13b30f3",
+    "url": "/my-app/static/media/pocketaudio_summary_bg02.4323a071.jpg"
+  }
+]);
